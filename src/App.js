93ce6036d01d1bf103d5ebfef7b4a2f9137fab92
Yash-Patel3 <nayangujar444@gmail.com>
2024-10-09 14:22:50 +0530

    CodeEditor is new component that is being added only issue is its not compiling the code

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './Components/WelcomePage'; 
 import CreateRoomPage from './Components/CreateRoomPage'; // Import the new CreateRoomPage component
 
+import CodeEditor from './Components/CodeEditor';
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/create-room" element={<CreateRoomPage />} /> {/* New route for CreateRoomPage */}
-        <Route path="/editor/:roomID" element={<h2>Room Page</h2>} /> {/* Placeholder for the room */}
+        <Route path="/editor/:roomId" element={<CodeEditor/>} /> {/* Placeholder for the room */}
       </Routes>
     </Router>
   );
